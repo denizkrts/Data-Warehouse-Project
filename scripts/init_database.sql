1d9d8b56608e7219d5131c63a5269f1547f0d161
denizkrts <161048970+denizkrts@users.noreply.github.com>
2025-02-26 00:51:44 +0300

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,19 @@
+/*  Create Database and Schemas
+                                 */
+-- Create the 'DataWarehouse' database
+
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
